eeb3b5c6bbf9273065e96e3fe3a96c63332645bb
Matt Scott <mattscott251@gmail.com>
2023-08-21 16:46:18 -0500

    completed all hash problems

--- hash-table.js
+++ hash-table.js
@@ -41,11 +41,61 @@ class HashTable {
   }
 
   insertWithHashCollisions(key, value) {
-    // Your code here
+    let keyStore = this.hashMod(key);
+    const newPair = new KeyValuePair(key, value);
+
+    if (this.data[keyStore] === null) {
+      this.data[keyStore] = newPair;
+      this.count++;
+    }
+    else {
+      // set keyvaluepair.next to equal the current value stored at array
+      newPair.next = this.data[keyStore];
+      // array key will point to new pair
+      this.data[keyStore] = newPair;
+
+      this.count++;
+    }
+
   }
 
   insert(key, value) {
-    // Your code here
+    // create new hashed index based on key
+    let hashKeyIndex = this.hashMod(key);
+    // create new keyValuePair based on key and value input
+    const newPair = new KeyValuePair(key, value);
+
+    // current array index is empty - add new key value pair
+    if (this.data[hashKeyIndex] === null) {
+      this.data[hashKeyIndex] = newPair;
+      this.count++;
+    }
+    // current array index is not empty
+    else {
+      // see if key already exists at index. If so, update value
+      // traverse linked list to do this
+      let current = this.data[hashKeyIndex];
+      let flag = 0;
+      while (current) {
+        if (current.key === key) {
+          current.value = value;
+          flag = 1;
+        }
+        current = current.next;
+      }
+      
+      // key does not exist, therefore, enable linked list chaining
+      if (flag === 0) {
+        // set keyvaluepair.next to equal the current value stored at array
+        newPair.next = this.data[hashKeyIndex];
+        // array key will point to new pair
+        this.data[hashKeyIndex] = newPair;
+        // update keyValuePair count
+        this.count++;
+      }
+
+    }
+
   }
 
 }
