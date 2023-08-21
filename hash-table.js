78011f4c7633e073bf06cf6e50848402d239ed30
Matt Scott <mattscott251@gmail.com>
2023-08-21 16:05:57 -0500

    completed half of problem set

--- hash-table.js
+++ hash-table.js
@@ -11,19 +11,33 @@ class KeyValuePair {
 class HashTable {
 
   constructor(numBuckets = 4) {
-    // Your code here
+    this.count = 0;
+    this.capacity = numBuckets;
+    this.data = new Array(numBuckets).fill(null);
   }
 
   hash(key) {
-    // Your code here
+    let hash = sha256(key);
+    hash = hash.slice(0,8);
+    return parseInt(hash,16);
   }
 
   hashMod(key) {
-    // Your code here
+    return (this.hash(key) % this.capacity);
   }
 
   insertNoCollisions(key, value) {
-    // Your code here
+    let keyStore = this.hashMod(key);
+    // check if element already at array index. if not, fill that index
+    if (this.data[keyStore] === null) {
+      const pair = new KeyValuePair(key, value);
+      this.data[keyStore] = pair;
+      this.count++;
+    }
+    else { // if array element at index, throw error
+      throw new Error('hash collision or same key/value pair already exists!');
+    }
+
   }
 
   insertWithHashCollisions(key, value) {
